5851341aa00f0d7315feadcc1a226f14de8f7dac
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-29 22:55:56 +0100

    Archivos actualizados

---
+++ ACT1/001children.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* Para cada una de las siguientes preguntas, da al menos una forma de cómo acceder a 
+ellos:
+¿El nodo <div> del DOM? */
+
+console.log(document.body.firstElementChild); // div, ya que el <div> es el primer hijo del body.
+
+// ¿El nodo <ul> del DOM?
+// ¿El segundo <li> (con Peter Parker)?
