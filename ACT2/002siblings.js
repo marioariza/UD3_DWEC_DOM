df6b2e54474cec2e2d15ad7332e4d0a4f19dabdf
*Mario <*mariojra6@gmail.com*>
2022-12-02 12:36:20 +0100

    Archivos actualizados

--- ACT2/002siblings.js
+++ ACT2/002siblings.js
@@ -7,11 +7,17 @@ let elem = document.getElementById("elem");
 alert(elem.lastChild.nextSibling);
 
 /* Sí. Al ser elem.lastChild se trata del último elemento, por lo tanto no puede
-haber un nextSibling */
+haber un nextSibling 
+
+Hago el ejemplo escribiendo el nodo "Mario". */
 
 // ---------------------------------
 
 // ¿Es cierto que elem.children[0].previousSibling siempre es null ?
 
-alert(elem.children[0].previousSibling);
+alert(elem.children[0].previousSibling.nodeValue);
+
+/* No, ya que aunque el hijo que seleccionemos sea el primero (children[0]), puede haber un nodo anterior a ese elemento.
+
+Hago el ejemplo escribiendo el nodo "Mario". */
 
