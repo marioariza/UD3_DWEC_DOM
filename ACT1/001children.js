1315700960bcd3618e895e79508ea38036da1d36
*Mario <*mariojra6@gmail.com*>
2022-12-02 12:28:33 +0100

    Archivos actualizados

--- ACT1/001children.js
+++ ACT1/001children.js
@@ -4,18 +4,23 @@
 ellos:
 ¿El nodo <div> del DOM? */
 
-let div = document.querySelector('div');
-div.style.color = "green";
-alert(div);
+let div = document.querySelector("div"); // querySelector("div") nos devuelve el elemento div del documento.
+let mostrar_div = div.outerHTML; // con outerHTML mostramos lo que contiene el div encontrado en el querySelector.
+alert(div + " : \n\n" + mostrar_div); // <div>Users:</div>
 
 // ¿El nodo <ul> del DOM?
 
-let ul = document.querySelector('ul');
-ul.style.color = "red";
-alert(ul);
+let ul = document.querySelector("ul"); // querySelector("ul") nos devuelve el elemento ul del documento.
+let mostrar_ul = ul.outerHTML; // con outerHTML mostramos lo que contiene el ul encontrado en el querySelector.
+alert(ul + " : \n\n" + mostrar_ul); 
+/*  <ul>
+        <li>Steve Rogers</li>
+        <li>Peter Parker</li>
+    </ul>
+*/
 
 // ¿El segundo <li> (con Peter Parker)?
 
-let li = ul.children[1];
-li.style.color = "blue";
-alert(li);
+let li = ul.children[1]; // con ul.children[1], encontramos el segundo hijo del elemento ul buscado en la línea 13.
+let mostrar_li = li.outerHTML; // con outerHTML mostramos lo que contiene el li encontrado en el children[1].
+alert(li + " : \n\n" + mostrar_li); // <li>Peter Parker</li>
--- ACT3/003diagonal.js
+++ ACT3/003diagonal.js
@@ -1,12 +1,9 @@
 "use strict";
 
-
 let table = document.body.firstElementChild;
 
-
-
-    for (let i = 0; i < table.rows.length; i++) {
-      let row = table.rows[i];
-      row.cells[i].style.backgroundColor = 'red';
-    }
+for (let i = 0; i < table.rows.length; i++) {
+  let row = table.rows[i];
+  row.cells[i].style.backgroundColor = 'red';
+}
 
