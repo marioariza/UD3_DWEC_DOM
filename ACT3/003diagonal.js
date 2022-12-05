2601a7772a322edae27ad33291c2909f8031dc34
*Mario <*mariojra6@gmail.com*>
2022-12-05 11:06:48 +0100

    Archivos actualizados

--- ACT3/003diagonal.js
+++ ACT3/003diagonal.js
@@ -2,13 +2,13 @@
 
 let trs = document.querySelectorAll('tr');
 let tds = document.querySelectorAll('td');
-let contador = 0;
+let pos = 0;
 
 for (let i = 0; i < tds.length; i++) {
   if (i > trs.length) {
-    tds[contador + trs.length + 1].style.backgroundColor= "red";
-    contador = contador + trs.length + 1;
+    pos += trs.length + 1;
+    tds[pos].style.backgroundColor= "red";
   } else {
-    tds[contador].style.backgroundColor = "red";
+    tds[pos].style.backgroundColor = "red";
   }
 }
