a1702d5a4edcea53ab83cba99dc557e17f1805af
*Mario <*mariojra6@gmail.com*>
2022-12-05 12:17:04 +0100

    Archivos actualizados

--- ACT3/003diagonal.js
+++ ACT3/003diagonal.js
@@ -1,16 +1,23 @@
 "use strict";
 
-let trs = document.querySelectorAll('tr');
-let tds = document.querySelectorAll('td');
-let pos = 0;
+// let trs = document.querySelectorAll('tr');
+// let tds = document.querySelectorAll('td');
+// let pos = 0;
 
-for (let i = 0; i < tds.length; i++) {
-  if (i > trs.length) {
-    pos += trs.length + 1;
-    tds[pos].style.backgroundColor= "red";
-    tds[pos].style.color = "white";
-  } else {
-    tds[pos].style.backgroundColor = "red";
-    tds[pos].style.color = "white";
-  }
-}
+// for (let i = 0; i < tds.length; i++) {
+//   if (i > trs.length) {
+//     pos += trs.length + 1;
+//     tds[pos].style.backgroundColor= "red";
+//     tds[pos].style.color = "white";
+//   } else {
+//     tds[pos].style.backgroundColor = "red";
+//     tds[pos].style.color = "white";
+//   }
+// }
+
+let table = document.body.firstElementChild;
+
+    for (let i = 0; i < table.rows.length; i++) {
+      let row = table.rows[i];
+      row.cells[i].style.backgroundColor = 'red';
+    }
