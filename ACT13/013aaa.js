65188bda0131279afb9ef8576be561f99faec015
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-08 14:00:15 +0100

    Archivos actualizados

---
+++ ACT13/013aaa.js
@@ -0,0 +1,9 @@
+"use strict"
+
+alert(table); // la tabla, tal como debería ser
+table.remove();
+// ¿Por qué aún está "aaa" en el documento?
+// Esto sucede debido a que solo coge las etiquetas específicas de la tabla y deja el texto 'aaa' fuera de ella.
+
+let aaa = table.children[0].previousSibling.nodeValue;
+alert('aaa');
