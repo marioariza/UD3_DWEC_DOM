5efcb9feb04e5640d87f5188f476cde4d787620e
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-15 23:09:14 +0100

    Archivos actualizados

--- ACT7/007tagInComment.js
+++ ACT7/007tagInComment.js
@@ -1,1 +1,9 @@
 "use strict";
+
+let body = document.body;
+body.innerHTML = "<!--" + body.tagName + "-->";
+alert(body.firstChild.data); // ¿qué hay aquí?
+
+/* BODY, ya que se reemplaza el contenido de body con el comentario que realizamos en el innerHTML.
+
+Entonces ahora el comentario es el unico hijo de body */
