1c073e00129d083d07f2d5f3198ee340ce17d3bf
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-15 23:20:39 +0100

    Archivos actualizados

---
+++ ACT8/008whereIsDocument.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* ¿A qué clase pertenece el document? */
+
+alert(document); // Pertenece a HTMLDocument.
+
+/* ¿Cuál es su lugar en la jerarquía DOM? */
+
+
+
+/* ¿Hereda de Node o Element, o tal vez HTMLElement? */
---
+++ ACT9/009getAttribute.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let elem = document.querySelector('[data-widget-name]');
+
+alert(elem.getAttribute('data-widget-name')); 
+
+// menu,cogemos el valor del atributo con getAttribute y especificando el nombre del atributo.
