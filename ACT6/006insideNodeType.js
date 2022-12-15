90074638c8d7ff82953fe6b05e79a0d6fd9e4f86
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-15 23:01:05 +0100

    Archivos actualizados

--- ACT6/006insideNodeType.js
+++ ACT6/006insideNodeType.js
@@ -1,3 +1,5 @@
 "use strict";
 
-alert(document.body.lastChild.nodeType); // 1
+alert(document.body.lastChild.nodeType); 
+
+// 1, ya que el nodeType muestra 1 cuando encuentra en la posición señalada un ELEMENT_NODE.
