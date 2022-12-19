089bca1102c87ec547889105c781b2df98715e4b
*Mario <*mariojra6@gmail.com*>
2022-12-19 13:33:47 +0100

    Archivos actualizados

--- ACT24/024collapsableMenu.js
+++ ACT24/024collapsableMenu.js
@@ -1,15 +1,15 @@
 "use strict";
 
-let boton = document.getElementById('boton');
+let boton = document.getElementById('boton'); // Recogemos el boton mediante el id.
 
 function desplegar() {
 
-    let contenido = boton.nextElementSibling;
+    let contenido = boton.nextElementSibling; // Recogemos el contenido que tenemos que desplegar
 
-    if (contenido.style.display === "block") {
+    if (contenido.style.display === "block") { // Si el contenido esta desplegado lo ocultamos.
         contenido.style.display = "none";
     } else { 
-        contenido.style.display = "block";
+        contenido.style.display = "block"; // Si el contenido esta oculto lo desplegamos.
     }
 }
 
