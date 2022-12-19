636fdff65f4458d41942c8d2b471200834e7f5cb
*Mario <*mariojra6@gmail.com*>
2022-12-19 13:03:31 +0100

    Archivos actualizados

---
+++ ACT22/022hideButton.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let boton = document.getElementById('boton'); // Recogemos el boton mediante el id.
+
+function borrar() {
+    boton.style.display = "none"; // Ocultamos el botón al hacer click.
+}
