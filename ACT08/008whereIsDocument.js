16a9024755080ba279529fb1e0a8fc6e34dc3ce0
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-21 17:13:04 +0100

    Archivos actualizados

--- ACT08/008whereIsDocument.js
+++ ACT08/008whereIsDocument.js
@@ -1,11 +1,15 @@
 "use strict";
 
+/* ¿Dónde está el "document" en la jerarquía? */
+
 /* ¿A qué clase pertenece el document? */
 
 alert(document); // Pertenece a HTMLDocument.
 
 /* ¿Cuál es su lugar en la jerarquía DOM? */
 
+// Están en el prototype del constructor. 
 
+alert(HTMLDocument.prototype.constructor === HTMLDocument); // true
 
 /* ¿Hereda de Node o Element, o tal vez HTMLElement? */
