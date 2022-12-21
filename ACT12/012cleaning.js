a354550834a2b0357b1de926710c2e5bf226e535
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-21 17:20:15 +0100

    Archivos actualizados

--- ACT12/012cleaning.js
+++ ACT12/012cleaning.js
@@ -1,11 +1,10 @@
 "use strict";
 
-let elem = document.querySelectorAll('li');
 clear(elem);
 
 function clear(elem) {
-    for (let el of elem) {
-        el.innerHTML='';
-    }
-    // Actualizamos el contenido del elemento con id = "elem" y lo ponemos vacío.
+
+    elem.innerHTML = ''; // Actualizamos el contenido del elemento con id = "elem" y lo ponemos vacío.
 }
+
+
