7a876628dc2704cad119c6d8835fd62cc37e4be7
Mario Rufo Ariza <mariojra6@gmail.com>
2022-12-21 19:20:28 +0100

    Archivos actualizados

---
+++ ACT25-26/025addCloseButton.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function borrar_horse () {
+    let horse = document.getElementById('horse'); // Recogemos el id del div.
+    horse.remove(); // Borramos el div recogido anteriormente.
+}
+
+function borrar_donkey () {
+    let donkey = document.getElementById('donkey');
+    donkey.remove();
+}
+
+function borrar_cat () {
+    let cat = document.getElementById('cat');
+    cat.remove();
+}
